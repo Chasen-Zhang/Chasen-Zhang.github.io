1aebdaf55938c20055ba9e24f5af1f7784d78396
张森泉 <zhang.sq@belle.com.cn>
2018-10-24 18:13:30 +0800

    f

---
+++ dist/precache-manifest.f04daad02b8d4e1918ea9fd615ff20e7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "4aaf4778567feb3ffd11",
+    "url": "/js/chunk-vendors.105edd5d.js"
+  },
+  {
+    "revision": "30e98b65b6b205bd5fab",
+    "url": "/js/app.daf589e5.js"
+  },
+  {
+    "revision": "a66aa29806c6a4dd7293",
+    "url": "/js/about.a8ca6209.js"
+  },
+  {
+    "revision": "7289ce79d4acddf762ed460829e29c3f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "30e98b65b6b205bd5fab",
+    "url": "/css/app.5f4249a6.css"
+  }
+];
